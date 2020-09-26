7336463dd0f96cc502ce820d76de9212f1a6d130
L9Caps <leaugeofbronze@gmail.com>
2020-09-26 19:04:26 -0400

    Global Chat Template and Style Created (No functionality yet)

--- src/components/MainChat.jsx
+++ src/components/MainChat.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import "../styles/MainChat.css"
+import React, { useState } from "react";
+import "../styles/MainChat.css";
 
 export default function MainChat() {
+  const [messagesList, setMessagesList] = useState([]);
+  const [currMessage, setCurrMessage] = useState("");
+  const [buttonState, setButtonState] = useState(false);
+
   return (
-    <div className="MainChat">
-        <h1>Develop Chat Section Here</h1>
+    <div className="MainChatBox">
+      <h3 className="chatTitle">Global Chat</h3>
+      <div className="innerChatDiv">{messagesList}</div>
+      <form className="sendMessageForm">
+        <input
+          type="text"
+          autoComplete="off"
+          placeholder="Type your message here!"
+          className="inputTextBox"
+          id="chatInput"
+        />
+        <button className="sendButton">{'Send >>'}</button>
+      </form>
     </div>
   );
 }
