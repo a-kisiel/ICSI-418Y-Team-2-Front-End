3db42418950cdd8ccb6407d45718d354033db648
L9Caps <leaugeofbronze@gmail.com>
2020-09-28 01:13:23 -0400

    Updated with loaded player cards

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 import "../styles/PlayerCard.css";
+import ProgressBar from "./ProgressBar";
+import { VisuallyHidden, ControlBox, Icon, Box } from "@chakra-ui/core";
 
 export default function PlayerCard(props) {
   return (
     <div className="PlayerCard">
       <img
         className="PlayerImage"
-        src="https://www.w3schools.com/w3css/img_snowtops.jpg"
+        src="https://www.pngitem.com/pimgs/m/508-5088889_work-in-progress-logo-png-transparent-png.png"
         alt="PlayerImage"
-        width="100%"
+        width="200px"
+        height="150px"
       />
+
       <div className="PlayerDescription">
-        <h2>name: {props.name}</h2>
-        <h2 style={{ margin: 0, padding: 0 }}>RP: {props.rankingpoints}</h2>
+        <h4 style={{ padding: 0, margin: 0 }}>{props.name}</h4>
+        <ProgressBar
+          statName="RP (Max 100)"
+          backgroundColor="black"
+          color="orange"
+          maxValue={100}
+          value={props.rankingPoints}
+        />
       </div>
+      <label>
+        <VisuallyHidden as="input" type="checkbox" defaultunChecked />
+        <ControlBox
+          id={`player${props.id}`}
+          style={{ border: "1px solid white" }}
+          borderWidth="1px"
+          size="24px"
+          rounded="sm"
+          _checked={{
+            bg: "red.500",
+            color: "white",
+            borderColor: "red.500",
+          }}
+          _focus={{ borderColor: "green.600", boxShadow: "outline" }}
+        >
+          <Icon name="check" size="16px" />
+        </ControlBox>
+
+        <Box as="span" verticalAlign="top" ml={3}>
+          Select Player
+        </Box>
+      </label>
     </div>
   );
 }
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -7,22 +7,30 @@ export default function ProgressBar(props) {
       style={{
         float: "left",
         width: "90%",
-        marginLeft: "5%",
-        marginRight: "5%",
-        padding: 0,
+        paddingLeft: "5%",
+        paddingRight: "5%",
         marginTop: 0,
-        marginBottom: "10px",
+        paddingBottom: "15px",
+        marginBottom: 0,
+        backgroundColor: props.backgroundColor,
       }}
     >
-      <h4 style={{ marginTop: "10px", padding: 0 }}>{props.statName}</h4>
+      <h4
+        style={{
+          marginTop: "10px",
+          backgroundColor: props.backgroundColor,
+        }}
+      >
+        {props.statName}
+      </h4>
       <div
         className="containerProgressBar"
         style={{
           width: "100%",
           border: "1px solid white",
           borderRadius: "20px",
-          padding: 0,
-          marginBottom: 0,
+          margin: 0,
+          backgroundColor: props.backgroundColor,
         }}
       >
         <div
@@ -31,7 +39,7 @@ export default function ProgressBar(props) {
             backgroundColor: props.color,
             width: `${(props.value / props.maxValue) * 100}%`,
             padding: 0,
-            margin: 0,
+            marginBottom: 0,
             borderRadius: "20px",
           }}
         >
